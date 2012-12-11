1ceb7268fbcd760625c9d4ebed389db0ccd7c0c9
Sachin Gandhi <gandhisi@gmail.com>
2012-12-10 20:12:58 -0800

    Adding Initial Code

---
+++ magicdelivery/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Magicdelivery::Application.config.secret_token = '66e622725d28f10ddddd7c47b54afd0dfce1b32dd47e087e1caa0291cf28269f6889b00b6e76a491d53df97aae9526df9cbd6b811fe638f9fe318233fa1938cb'
